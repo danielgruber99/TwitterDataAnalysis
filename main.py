4ba256191c84bcf59d0eb3d0ad9317a0c622b23b
Daniel Gruber <daniel.reisbach@gmx.de>
2022-04-25 07:39:05 +0200

    utilize os module for loading env vars

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import os
 import tweepy
+import pandas
+import csv
 
+access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
+print(bearer_token)
+#client = tweepy.Client(bearer_token=bearer_token,
+#consumer_key = consumer_key, 
+#consumer_secret = consumer_secret,
+#access_token = access_token,
+#access_token_secret= access_token_secret,)
+client = tweepy.Client(bearer_token=bearer_token)
 
-#client = tweepy.Client(bearer_token="AAAAAAAAAAAAAAAAAAAAABqKbgEAAAAAS2RKvDrWSBI4OGvmZz8Qqe%2Bmg%2F0%3D20aat5g0H71Cj1yiBDTyAJSENfzO3cYZ3VWCJfnWywIC7OiIZz",
-#consumer_key = "fm0EuJ83rk0r843ibjlGnTiW4",
-#consumer_secret = "0O0viPgphDri25PdwWqjf1RED6mmfxDqfXeWBDYqDcouLPPooA",
-#access_token = "2904125878-WGdmAvONtSBtaoebTKu98ut9qDCeIi66FJrBCkL",
-#access_token_secret="Xad9Ny5BgL6icB4hPC3ggQIodU9QmernG8ibpI1Flqr0q")
-client = tweepy.Client(bearer_token="AAAAAAAAAAAAAAAAAAAAABqKbgEAAAAAS2RKvDrWSBI4OGvmZz8Qqe%2Bmg%2F0%3D20aat5g0H71Cj1yiBDTyAJSENfzO3cYZ3VWCJfnWywIC7OiIZz")
-
-tweets = client.search_recent_tweets(query="soccer", max_results=10)
+tweets = client.search_recent_tweets(query="computer", max_results=10)
 print(tweets)
 
 #auth = tweetpy.OAuthHandler(consumer_key, consumer_api, callback_uri)
