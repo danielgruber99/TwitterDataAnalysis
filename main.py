6ea4bf843788c9ef75769056b79efc59608220d8
danieldagruber <daniel.reisbach@gmx.de>
2022-04-26 12:37:18 +0200

    fetch response.data for tweets and store in csv
    This includes basic functionality to fetch and store data
    so currently tweet id, tweet text, tweet created_at and
    the author_id are fetched and stored to a csv file called
    result.csv.

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def main():
     consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
     consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
     bearer_token = os.environ.get("TWITTER_BEARER_TOKEN")
-    print(bearer_token)
     #client = tweepy.Client(bearer_token=bearer_token,
     #consumer_key = consumer_key, 
     #consumer_secret = consumer_secret,
@@ -20,12 +19,24 @@ def main():
     #access_token_secret= access_token_secret,)
     client = tweepy.Client(bearer_token=bearer_token)
 
-    tweets = client.search_recent_tweets(query="computer", max_results=10)
+    response = client.search_recent_tweets(query="computer", tweet_fields=["created_at", "lang"], expansions=["author_id"], max_results=10)
+    tweets = response.data
+   
+    csvFile = open('result.csv','w')
+    csvWriter = csv.writer(csvFile)
 
+    csvWriter.writerow(["Tweet ID", "Tweet Text", "Tweet created_at", "user_id" ])
+    for tweet in tweets:
+
+        # Write a row to the CSV file. I use encode UTF-8
+        csvWriter.writerow([tweet.id, tweet.text, tweet.created_at, tweet.author_id])
+        #print tweet.created_at, tweet.text
+    csvFile.close()
+            
 
 
     print(tweets)
-    #auth = tweepy.OAuth2BearerHandler('AAAAAAAAAAAAAAAAAAAAABqKbgEAAAAAS2RKvDrWSBI4OGvmZz8Qqe%2Bmg%2F0%3D20aat5g0H71Cj1yiBDTyAJSENfzO3cYZ3VWCJfnWywIC7OiIZz')
+    
 
 if __name__ == "__main__":
     main()
