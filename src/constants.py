f984cbd36e56e3c7c6794933ce34dd0920c7fd11
danieldagruber <daniel.reisbach@gmx.de>
2022-05-29 10:55:33 +0200

    beautify constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,8 @@
 # constants
+# =========
 
 # --------------------------------------------------------
-# dataframe tweets
+# DATAFRAME tweets
 tweet_id = "Tweet ID"
 tweet_text = "Tweet text"
 tweet_entities = "Tweet entities"
@@ -9,13 +10,13 @@ tweet_createdAt = "Tweet created at"
 tweet_hashtags = "hashtags"
 
 # --------------------------------------------------------
-# dataframe users
+# DATAFRAME users
 user_id = "User ID"
 user_name = "name"
 user_username = "username"
 
 # --------------------------------------------------------
-# dataframe followers
+# DATAFRAME followers
 follower_id = "Follower ID"
 follower_name = "name"
 follower_username = "username"
@@ -28,19 +29,20 @@ follower_followers = "Followers"
 follower_url = "URL"
 follower_profile_image_url = "profile image url"
 
-# dataframe followers_tweets
+# --------------------------------------------------------
+# DATAFRAME followers_tweets
 # follower_id as in dataframe followers
 follower_tweet_id = "Tweet ID"
 follower_tweet_text = "Tweet text"
 
 # --------------------------------------------------------
+# CONSTANT NUMBERS
 # number of shown entries per page
 NR_ENTRIES_PAGE = 20
 # Number of tweets to fetch
 NR_TWEETS = 100
 # Number of followers of given userid to fetch
 NR_FOLLOWERS = 500
-
 # Number of followers to fetch tweets for (only those first followers are considered and tweets are fetched only for those)
 NR_FOLLOWERS_FOR_TWEETS = 50 - 1  # as indexing begins from zero, 0 - 49 are 50 elements
 # Number of followers tweets to fetch
